26e857909f2bcadf1135f8944346100d627e344e
krish327 <ks355965@gmail.com>
2020-11-06 12:39:35 +0530

    shinchan

--- sketch.js
+++ sketch.js
@@ -1,11 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
-
+var log,chain1
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
@@ -14,7 +15,6 @@ function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
-
 
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
@@ -35,7 +35,8 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-
+    log=new Log(230,200,80,PI/2)
+    chain1=new chain(bird.body,log.body)
 }
 
 function draw(){
@@ -61,4 +62,6 @@ function draw(){
 
     bird.display();
     platform.display();
+    chain1.display()
+    log.display()
 }
